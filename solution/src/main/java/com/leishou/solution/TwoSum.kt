fa502e4bdfd2e58303afb3af69879b547753e525
Lei <st115236@hotmail.com>
2026-01-01 04:05:20 +0800

    TwoSum

---
+++ solution/src/main/java/com/leishou/solution/TwoSum.kt
@@ -0,0 +1,17 @@
+package com.leishou.solution
+
+class TwoSum {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+        nums.forEachIndexed { index, i ->
+            val diff = target - i
+            if (map.contains(diff) && map[diff] != index) {
+                return intArrayOf(index, map[diff]!!).sortedArray()
+            } else {
+                map[i] = index
+            }
+        }
+
+        return intArrayOf()
+    }
+}
---
+++ solution/src/test/java/com/leishou/solution/TwoSumTest.kt
@@ -0,0 +1,27 @@
+package com.leishou.solution
+
+import org.junit.Assert.assertArrayEquals
+import org.junit.Test
+
+class TwoSumTest {
+    @Test
+    fun testExample1() {
+        val sol = TwoSum()
+        val result = sol.twoSum(intArrayOf(2, 7, 11, 15), 9)
+        assertArrayEquals(intArrayOf(0, 1), result)
+    }
+
+    @Test
+    fun testExample2() {
+        val sol = TwoSum()
+        val result = sol.twoSum(intArrayOf(3, 2, 4), 6)
+        assertArrayEquals(intArrayOf(1, 2), result)
+    }
+
+    @Test
+    fun testExample3() {
+        val sol = TwoSum()
+        val result = sol.twoSum(intArrayOf(3, 3), 6)
+        assertArrayEquals(intArrayOf(0, 1), result)
+    }
+}
